178bc0fcba81a43072d182688bdf3f4318b91f5e
Hehesheng <hhslzzyyes@outlook.com>
2018-09-01 08:37:06 +0800

    v1.1.2

--- SYSTEM/usart/usart.h
+++ SYSTEM/usart/usart.h
@@ -4,7 +4,13 @@
 #include "sys.h"
 #include "stdio.h"
 
+#define scanfSupport 0       //是否支持scanf
+#define specialPrint 0       //是否启用特殊printf, 即fflush()
+#define USART_REC_LEN 128    //定义最大接收字节数 128
+#define USART_RX_OK (0X8000) //接受成功标志位
+
 #ifdef __GNUC__
+#if specialPrint
 #define printf(format, str...) \
     do                         \
     {                          \
@@ -12,10 +18,7 @@
         fflush(stdout);        \
     } while (0)
 #endif
-
-#define scanfSupport 0      //是否支持scanf
-#define USART_REC_LEN 128    //定义最大接收字节数 128
-#define USART_RX_OK (0X8000) //接受成功标志位
+#endif
 
 extern u8 USART_RX_BUF[USART_REC_LEN]; //接收缓冲,最大USART_REC_LEN个字节.末字节为换行符
 extern u16 USART_RX_STA;               //接收状态标记
